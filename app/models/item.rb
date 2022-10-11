0ac14c8103c688f04ab539996d28529a2739f664
reiyasawada <108113759+reiyasawada@users.noreply.github.com>
2022-10-11 22:57:24 +0900

    商品出品機能修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -25,7 +25,7 @@ class Item < ApplicationRecord
     validates :prefecture_id
     validates :arrival_day_id
     # 300円以上かつ9,999,999円以下で、半角数字でないと入力不可
-    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   end
 
   # ジャンルの選択が「--」の時は保存不可
