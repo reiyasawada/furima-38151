96ef3845e1a74ef3665f6ca6c91e313f1fd2080a
reiyasawada <108113759+reiyasawada@users.noreply.github.com>
2022-10-11 21:15:50 +0900

    商品出品機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,27 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
+  
   def index
-    # @items = Item.all.includes(:user).order('created_at DESC')
+    @items = Item.all.includes(:user).order('created_at DESC')
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :explanation, :category_id, :condition_id, :delivery_charge_id, :prefecture_id,
+                                 :arrival_day_id, :price).merge(user_id: current_user.id)
   end
 end
---
+++ app/models/item.rb
@@ -0,0 +1,39 @@
+class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
+  # テーブルとのアソシエーション
+  belongs_to :user
+
+  # アクティブハッシュとのアソシエーション
+  belongs_to :category
+  belongs_to :prefecture
+  belongs_to :condition
+  belongs_to :delivery_charge
+  belongs_to :arrival_day
+
+  # active_storageとのアソシエーション
+  # （items・active_storage_blobsテーブルを関連付け）
+  has_one_attached :image
+
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :explanation
+    validates :category_id
+    validates :condition_id
+    validates :delivery_charge_id
+    validates :prefecture_id
+    validates :arrival_day_id
+    # 300円以上かつ9,999,999円以下で、半角数字でないと入力不可
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  end
+
+  # ジャンルの選択が「--」の時は保存不可
+  with_options numericality: { other_than: 0 } do
+    validates :category_id
+    validates :prefecture_id
+    validates :condition_id
+    validates :delivery_charge_id
+    validates :arrival_day_id
+  end
+end
