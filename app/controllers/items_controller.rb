61c095e53dea82f125c8e8aeb4880683dfdcf691
reiyasawada <108113759+reiyasawada@users.noreply.github.com>
2022-10-16 14:58:40 +0900

    再修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,8 +20,8 @@ class ItemsController < ApplicationController
     end
   end
 
-  #def show
-  #end
+  def show
+  end
 
   private
 
@@ -30,7 +30,7 @@ class ItemsController < ApplicationController
                                  :arrival_day_id, :price).merge(user_id: current_user.id)
   end
 
-  #def set_item
-    #@item = Item.find(params[:id])
-  #end
+  def set_item
+    @item = Item.find(params[:id])
+  end
 end
