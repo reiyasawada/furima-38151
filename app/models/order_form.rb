f6d68581f8f5ab91edbf3eef489443b87c31a4fb
reiyasawada <108113759+reiyasawada@users.noreply.github.com>
2022-10-27 14:57:20 +0900

    商品購入機能の実装 修正

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -9,7 +9,7 @@ class OrderForm
     validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
     validates :city
     validates :street_number
-    validates :phone_number, format: { with: /\A[0-9]{10 or 11}\z/, message: 'is invalid' }
+    validates :phone_number, format: { with: /\A[0-9]{10,11}\z/, message: 'is invalid' }
     validates :token
   end
 
